dbd0e87f2267a9289bccdc0931e929672cae4f72
AzuMaooo <katyj147@gmail.com>
2026-08-19 15:53:43 +0800

    Add quantity selector on product page, success message with fade animation

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.view_cart, name='view_cart'),
+    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cart/update/<int:product_id>/', views.update_cart, name='update_cart'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product 
+from django.contrib import messages
 
 def product_list(request):
     products = Product.objects.all()
@@ -12,15 +13,47 @@ def product_detail(request, product_id):
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     cart = request.session.get('cart', {})
-
     product_id_str = str(product_id)
+
+    new_quantity = int(request.POST.get('quantity'))
+
     if product_id_str in cart:
-        cart[product_id_str] += 1
+        cart[product_id_str] += new_quantity
+        
     else:
-        cart[product_id_str] = 1
+        cart[product_id_str] = new_quantity
 
     request.session['cart'] = cart
+    messages.success(request, f'Added {new_quantity}x {product.name} to cart!')
     return redirect('product_list')
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    product_id_str = str(product_id)
+
+    if product_id_str in cart:
+        del cart[product_id_str]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+def update_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    product_id_str = str(product_id)
+
+    if request.method == 'POST':
+        new_quantity = int(request.POST.get('quantity'))
+
+        if new_quantity <= 0:
+            if product_id_str in cart:
+                del cart[product_id_str]
+        else:
+            cart[product_id_str] = new_quantity
+
+        request.session['cart'] = cart
+
+    return redirect('view_cart')
 
 def view_cart(request):
     cart = request.session.get('cart', {})
